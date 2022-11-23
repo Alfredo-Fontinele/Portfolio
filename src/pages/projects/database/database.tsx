c6f81d322b6649e49b3f25d9bd13e31f1abfbc08
Alfredo <alfredofontinele08@gmail.com>
2022-11-23 09:36:55 -0300

    refactor/imgs and database projects

--- src/pages/projects/database/database.tsx
+++ src/pages/projects/database/database.tsx
@@ -1,8 +1,8 @@
 export const DATABASE_PROJECTS = [
     {
-        name: "Movies",
-        image: "https://raw.githubusercontent.com/Alfredo-Fontinele/Projects_Imgs/main/filmes.png",
-        url: "https://catalogo-filmes-iota.vercel.app/"
+        name: "SongUp",
+        image: "https://raw.githubusercontent.com/Alfredo-Fontinele/Projects_Imgs/main/songup.png",
+        url: "https://song-up-alfredo-fontinele.vercel.app/"
     },
     {
         name: "Foodbox",
@@ -15,9 +15,9 @@ export const DATABASE_PROJECTS = [
         url: "https://password-generator-alfredo-fontinele.vercel.app/"
     },
     {
-        name: "Harry Potter",
-        image: "https://raw.githubusercontent.com/Alfredo-Fontinele/Projects_Imgs/main/harry_potter.png",
-        url: "https://harry-potter-sigma.vercel.app/"
+        name: "Shop Now",
+        image: "https://raw.githubusercontent.com/Alfredo-Fontinele/Projects_Imgs/main/shop_now.png",
+        url: "https://kenzie-academy-brasil-developers.github.io/entrega-lista-produtos-m2-sprint-1b-Alfredo-Fontinele/"
     },
     {
         name: "Kenzie Clone",
@@ -25,38 +25,18 @@ export const DATABASE_PROJECTS = [
         url: "https://clone-kenzy-academy.vercel.app/"
     },
     {
-        name: "Kenzie Blog",
-        image: "https://raw.githubusercontent.com/Alfredo-Fontinele/Projects_Imgs/main/kenzie_blog.png",
-        url: "https://kenzie-academy-brasil-developers.github.io/m1_sprint1_entrega_blog-Alfredo-Fontinele/"
-    },
-    {
-        name: "Kenzie Matérias",
-        image: "https://raw.githubusercontent.com/Alfredo-Fontinele/Projects_Imgs/main/kenzie_materias.png",
-        url: "https://materias-kenzie.vercel.app/"
-    },
-    {
         name: "Kenzie Posts",
         image: "https://raw.githubusercontent.com/Alfredo-Fontinele/Projects_Imgs/main/kenzie_posts.png",
         url: "https://posts-news-kenzie.vercel.app/"
     },
     {
-        name: "Calculadora",
-        image: "https://raw.githubusercontent.com/Alfredo-Fontinele/Projects_Imgs/main/calculadora.png",
-        url: "https://calculadora-grid-beta.vercel.app/"
-    },
-    {
-        name: "Shop Now",
-        image: "https://raw.githubusercontent.com/Alfredo-Fontinele/Projects_Imgs/main/shop_now.png",
-        url: "https://kenzie-academy-brasil-developers.github.io/entrega-lista-produtos-m2-sprint-1b-Alfredo-Fontinele/"
-    },
-    {
-        name: "Tech Shop",
-        image: "https://raw.githubusercontent.com/Alfredo-Fontinele/Projects_Imgs/main/techshop.png",
-        url: "https://tech-shop-kenzie-academy.vercel.app/"
-    },
-    {
         name: "Todo List",
         image: "https://raw.githubusercontent.com/Alfredo-Fontinele/Projects_Imgs/main/todolist.png",
         url: "https://todo-list-plum-one.vercel.app/"
+    },
+    {
+        name: "Calculadora",
+        image: "https://raw.githubusercontent.com/Alfredo-Fontinele/Projects_Imgs/main/calculadora.png",
+        url: "https://calculadora-grid-beta.vercel.app/"
     }
 ]
