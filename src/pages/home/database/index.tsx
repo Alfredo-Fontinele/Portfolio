64e9aeb6592d0d673e44bababd1726713e5ec2ad
Alfredo <alfredofontinele08@gmail.com>
2023-04-12 19:52:23 -0300

    feat : rename images technologies and texts

--- src/pages/home/database/index.tsx
+++ src/pages/home/database/index.tsx
@@ -1,18 +1,19 @@
-import ReactJS from "../../../../src/assets/technologies/React-icon.svg.png";
+import ReactJS from "../../../../src/assets/technologies/react.svg.png";
 import NextJS from "../../../../src/assets/technologies/next-js.svg";
 import ChakraUI from "../../../../src/assets/technologies/chakraui.svg";
 import NodeJS from "../../../../src/assets/technologies/nodejs.png";
 import Git from "../../../../src/assets/technologies/git.png";
 import Django from "../../../../src/assets/technologies/django.png";
 import Jest from "../../../../src/assets/technologies/jest.png";
-import TypeScript from "../../../../src/assets/technologies/Typescript_logo_2020.svg.png";
-import PostgreSQL from "../../../../src/assets/technologies/postgresql-icon.webp";
+import TypeScript from "../../../../src/assets/technologies/typescript.svg.png";
+import PostgreSQL from "../../../../src/assets/technologies/postgresql.webp";
 import TypeORM from "../../../../src/assets/technologies/typeorm.jpg";
 import Prisma from "../../../../src/assets/technologies/prisma.png";
 import Tailwind from "../../../../src/assets/technologies/tailwind.png";
 import Docker from "../../../../src/assets/technologies/docker.png";
 import NestJS from "../../../../src/assets/technologies/nestjs.png";
 import Express from "../../../../src/assets/technologies/express.png";
+import MongoDB from "../../../../src/assets/technologies/mongo-db.png";
 
 export interface ITech {
     name: string;
@@ -37,12 +38,12 @@ export const DATABASE: ITech[] = [
         image: ChakraUI,
     },
     {
-        name: "Git",
-        image: Git,
-    },
-    {
         name: "Nest.js",
         image: NestJS,
+    },
+    {
+        name: "Node.js",
+        image: NodeJS,
     },
     {
         name: "TypeScript",
@@ -61,10 +62,6 @@ export const DATABASE: ITech[] = [
         image: Prisma,
     },
     {
-        name: "Node.js",
-        image: NodeJS,
-    },
-    {
         name: "Express.js",
         image: Express,
     },
@@ -76,16 +73,16 @@ export const DATABASE: ITech[] = [
         name: "Jest",
         image: Jest,
     },
-    // {
-    //     name: 'MongoDB',
-    //     image: 'https://devkico.itexto.com.br/wp-content/uploads/2013/10/mongodb-leaf.png'
-    // },
-    // {
-    //     name: 'Amazon AWS',
-    //     image: 'https://user-images.githubusercontent.com/51785898/91358419-31502480-e7c9-11ea-9bb8-5124117e9a75.png'
-    // },
+    {
+        name: "MongoDB",
+        image: MongoDB,
+    },
     {
         name: "Django",
         image: Django,
+    },
+    {
+        name: "Git",
+        image: Git,
     },
 ];
--- src/pages/page-not-found/index.tsx
+++ src/pages/page-not-found/index.tsx
@@ -3,7 +3,7 @@ import * as S from "./style";
 export const PageNotFound = () => {
     return (
         <S.PageNotFound>
-            <S.Text>Page Not Found</S.Text>
+            <S.Text>Página Inexistente</S.Text>
             <S.Button to="/">Voltar pra Home</S.Button>
         </S.PageNotFound>
     );
